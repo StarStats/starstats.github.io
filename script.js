bc5bd738df7b87e65ce91e1dcaefda86563cf215
Sebastian Doe <sebastiandoe5@gmail.com>
2019-11-10 16:03:18 +0000

    Add main site

---
+++ script.js
@@ -0,0 +1,17 @@
+function githubRepository() {
+    window.location.href = "github/repository/index.html"
+}
+
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(location.search) || [null, ""])[1].replace(/\+/g, "%20")) || null;
+}
+
+function githubRepo() {
+    var openURL = "stats.html?user=" + encodeURIComponent(document.getElementById("githubUser").value) + "&repo=" + encodeURIComponent(document.getElementById("githubRepo").value) + "&SameSite=Secure";
+    window.location.href = openURL;
+}
+
+function githubRepoLoad() {
+    var GitHubUser = getURLParameter("user");
+    var GitHubRepo = getURLParameter("repo");
+    var jsonURL = "https://api.github.com/repos/" + GitHubUser + "/" + GitHubRepo;
